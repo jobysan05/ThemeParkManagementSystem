da0e14eb9643e80a2db74dcb7b389d7a7f7b6fc3
Matt Potter <mattpotter28@gmail.com>
2018-04-17 14:22:58 -0500

    owin packages added and startup.cs added

---
+++ ThemeParkManagementSystem/Startup1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using System.Web.Helpers;
+using System.Security.Claims;
+
+[assembly: OwinStartup(typeof(ThemeParkManagementSystem.Startup1))]
+
+namespace ThemeParkManagementSystem
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/LogOn")
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+        }
+    }
+}
