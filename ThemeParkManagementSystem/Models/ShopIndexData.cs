ce01c401781faa27c786c4afd95896e9b56a6abc
Brandon Le <DJ.ble07@gmail.com>
2018-04-23 15:12:34 -0500

    Display related data in shops page and implement search function

---
+++ ThemeParkManagementSystem/Models/ShopIndexData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThemeParkManagementSystem.Models
+{
+    public class ShopIndexData
+    {
+        public IEnumerable<ThemeParkManagementSystem.Models.INVENTORY> Inventory { get; set; }
+        public IEnumerable<ThemeParkManagementSystem.Models.SHOP> Shops { get; set; }
+    }
+}
