df418518b2f51122f8ca6e83aa5f856a47ff5e06
Jeffrey Pernia <jeffreypernia@gmail.com>
2018-04-30 13:48:43 -0500

    includes Shop revenue report

--- ThemeParkManagementSystem/Controllers/ShopRevenueController.cs
+++ ThemeParkManagementSystem/Controllers/ShopRevenueController.cs
@@ -4,60 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ThemeParkManagementSystem.Models;
-using System.Data.Entity;
+
+
+
 
 namespace ThemeParkManagementSystem.Controllers
 {
-    public class ReportsController : Controller
+    
+    public class ShopRevenueController : Controller
     {
-        // GET: Reports
         private tpdatabaseEntities db = new tpdatabaseEntities();
-        public ActionResult Index(DateTime? date1, DateTime? date2, int? id)
+        // GET: ShopRevenue
+        public ActionResult Index(DateTime? date1, DateTime? date2)
         {
+            Nullable<decimal> countlist = db.shoprevenue(date1, date2).ToList<Nullable<decimal>>().FirstOrDefault();
 
-
-            if (id == null)
-            {
-                Nullable<int> popularRideID = db.topRides(date1, date2).ToList<Nullable<int>>().FirstOrDefault();
-
-                var popularID = popularRideID;
-
-                DateTime? d1 = date1;
-                DateTime? d2 = date2;
-                Nullable<int> numOfGuest = db.Guestcount(d1, d2, popularID).ToList<Nullable<int>>().FirstOrDefault();
-                var guestCount = numOfGuest.Value;
-
-                var result = "The ID for the most popular ride is  " + popularID + " and it had " + numOfGuest + " guests";
-
-                ViewData["Result"] = result;
-                
-                return View();
-
-            }
-            else
-            {
-                Nullable<int> countList = db.RideCount(date1, date2, id).ToList<Nullable<int>>().FirstOrDefault();
-                var rideCount = countList.Value;
-
-                var resultt = "Ride " + id + " had  " + rideCount + " guests between the choosen dates";
-                ViewData["Result"] = resultt;
-            }
+            var count = countlist;
+            ViewData["Rev"] = count;
             return View();
+            
         }
 
-        // GET: Reports/Details/5
+        // GET: ShopRevenue/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: s/Create
+        // GET: ShopRevenue/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: s/Create
+        // POST: ShopRevenue/Create
         [HttpPost]
         public ActionResult Create(FormCollection collection)
         {
@@ -73,13 +53,13 @@ namespace ThemeParkManagementSystem.Controllers
             }
         }
 
-        // GET: s/Edit/5
+        // GET: ShopRevenue/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: s/Edit/5
+        // POST: ShopRevenue/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
@@ -95,13 +75,13 @@ namespace ThemeParkManagementSystem.Controllers
             }
         }
 
-        // GET: s/Delete/5
+        // GET: ShopRevenue/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: s/Delete/5
+        // POST: ShopRevenue/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
